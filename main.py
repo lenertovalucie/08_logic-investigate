8827e94631fab0e672c4df20b3936cfc20a65b19
Lucie Lénertová <lenertova.lucie@gmail.com>
2022-08-19 19:02:02 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,18 @@
+birthMonth = int(input("Enter the number of the month in which you were born. \n"))
+
+if birthMonth >= 1 and birthMonth <=12:
+    print("Thanks")
+else:
+    print("That's not a valid month!")
+
+# Jaký logický operátor je použit v tomto programu?
+    #
+
+# Co by se stalo pokud bych and napsala velkými písmeny?
+    #
+
+# Co by se stalo, pokud bych místo >= 1 použila > 1? Nápověda: Upravte program a otestujte jej s hodnotou 0 a 1 a 2.
+    #
+
+# Co by ses stalo, pokud bych místo operátoru and použila operátor or? Nápověda: Upravte program a otestujte jej s různými číslicemi.
+    #
